c7087f05abfd480bb01d7f154514127aa5733954
Taren Gorman <tarengorman@gmail.com>
2017-12-21 00:16:32 -0800

    simple nav bar with css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,6 +7,11 @@ class Header extends Component {
     return (
       <header>
         <h1 className="Header-title">Taren Gorman</h1>
+        <ul>
+          <li><a href="/">Contact</a></li>
+          <li><a href="/">Projects</a></li>
+          <li><a href="/">About</a></li>
+        </ul>
       </header>
     );
   }
