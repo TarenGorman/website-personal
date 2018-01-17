50af14164456a27ecf0f7fd0f5ea1c5536c79aac
Taren Gorman <tarengorman@gmail.com>
2018-01-16 19:15:32 -0800

    updated about page with pdf resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import * as THREE from 'three';
+import { Document, Page } from 'react-pdf';
+import MediaQuery from 'react-responsive';
 import './index..css';
 import Construction from '../Construction';
 import Footer from '../Footer';
@@ -13,12 +14,19 @@ class About extends Component {
           <div className="picture"></div>
         </div>
         <div className="about-container">
-          <p> Hi I'm Taren Gorman, I graduated from University of Washington in 2017 with a B.S. in Applied Physics. Currently I'm looking to join a company to further apply my skills in programming and engineering.
-          I am especially interested in projects involving machine learning, real-time systems, and VR/AR. Please feel free to contact me with any opportunities.</p>
+          <MediaQuery minDeviceWidth={1115} component="p" className="p">
+          Hi I'm Taren Gorman, I graduated from University of Washington in 2017 with a B.S. in Applied Physics. Currently I'm looking to join a company to further apply my skills in programming and engineering.
+          I am especially interested in projects involving machine learning, real-time systems, and VR/AR. Please feel free to contact me with any opportunities.
+          </MediaQuery>
+          <MediaQuery maxDeviceWidth={1115} component="p" className="p-mobile">
+          Hi I'm Taren Gorman, I graduated from University of Washington in 2017 with a B.S. in Applied Physics. Currently I'm looking to join a company to further apply my skills in programming and engineering.
+          I am especially interested in projects involving machine learning, real-time systems, and VR/AR. Please feel free to contact me with any opportunities.
+          </MediaQuery>
         </div>
         <div className="resume-container">
           <h1> Resume </h1>
-          <div className="resume"></div>
+          <Resume/>
+          <h2><a href={require("../../images/primary_resume.pdf")} download="Taren_Gorman_Resume_2018"> Download Resume </a></h2>
         </div>
         <div className="flight-container">
           <Construction/>
@@ -27,6 +35,23 @@ class About extends Component {
       </div>
     );
   }
+}
+
+const Resume = ({ }) => {
+  return (
+    <div>
+      <MediaQuery minDeviceWidth={1115} component="div" className="resume">
+        <Document file={require("../../images/primary_resume.pdf")}>
+          <Page pageNumber={1} width={800}/>
+        </Document>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={1115} component="div" className="resume-mobile">
+        <Document file={require("../../images/primary_resume.pdf")}>
+          <Page pageNumber={1} width={window.innerWidth*0.90}/>
+        </Document>
+      </MediaQuery>
+    </div>
+  );
 }
 
 export default About;
